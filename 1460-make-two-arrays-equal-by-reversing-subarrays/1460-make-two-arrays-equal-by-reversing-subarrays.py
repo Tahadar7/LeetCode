6c84cdef3148f9e57bc64eabf5ce65c1b401c065
Taha Manshoor Dar <tahadar221177@gmail.com>
2026-07-01 13:28:41 +0500

    Time: 7 ms (10.93%), Space: 19.4 MB (14.63%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.py
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.py
@@ -1,8 +1,5 @@
 class Solution:
     def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
-        if not target or not arr:
-            return False
-
         # Counter: Dictionary mapping per time items
         return Counter(target) == Counter(arr)  
         
