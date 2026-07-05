daf7398d270988b04d0008da801f39150ef6a737
Taha Manshoor Dar <tahadar221177@gmail.com>
2026-07-05 11:59:26 +0500

    Time: 0 ms (100%), Space: 19.3 MB (0%) - LeetHub

--- 3978-unique-middle-element/3978-unique-middle-element.py
+++ 3978-unique-middle-element/3978-unique-middle-element.py
@@ -1,7 +1,5 @@
 class Solution:
     def isMiddleElementUnique(self, nums: list[int]) -> bool:
-        if not nums:
-            return False
         mid = (len(nums) - 1)//2
 
         for i in range(len(nums)):
