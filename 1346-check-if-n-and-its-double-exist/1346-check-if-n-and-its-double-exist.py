c1be1c76d66d8e3df9ca14a5af4549b3ee9df514
Taha Manshoor Dar <tahadar221177@gmail.com>
2026-07-26 15:40:52 +0500

    Time: 0 ms (100%), Space: 19.2 MB (63.34%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -1,9 +1,8 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
         seen = set()        # values accessed as hashTable
-        for num in arr:
-            # if double in set or its exactly half in set 
-            if 2 * num in seen or (num // 2 in seen and num % 2 == 0):
+        for num in arr: 
+            if 2 * num in seen or (num // 2 in seen and num % 2 == 0):              # if double in set or its exactly half in set
                 return True
             seen.add(num)
         return False
